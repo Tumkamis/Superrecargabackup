217495cd126b2a9c3341b8ac0e84a530977faa12
Tumkamis <82539574+Tumkamis@users.noreply.github.com>
2021-05-18 10:48:42 -0500

    cambio base

--- application/config/database.php
+++ application/config/database.php
@@ -65,9 +65,9 @@ $active_record = TRUE;
 //$db['default']['autoinit'] = TRUE;
 //$db['default']['stricton'] = FALSE;
 
-$db['default']['hostname'] = '54.183.74.230';
+$db['default']['hostname'] = '54.176.233.121';
 $db['default']['username'] = 'Diatel';
-$db['default']['password'] = 'Diatel2020';
+$db['default']['password'] = 'diatel2020';
 $db['default']['database'] = 'superrecargafundacion';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
