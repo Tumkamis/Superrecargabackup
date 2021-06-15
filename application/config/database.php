527dc38cf0ceb87ea120567660098b902eea6489
Tumkamis <82539574+Tumkamis@users.noreply.github.com>
2021-06-15 14:51:20 -0500

    Update database.php
    cambiodatos backup

--- application/config/database.php
+++ application/config/database.php
@@ -65,9 +65,9 @@ $active_record = TRUE;
 //$db['default']['autoinit'] = TRUE;
 //$db['default']['stricton'] = FALSE;
 
-$db['default']['hostname'] = '54.176.233.121';
+$db['default']['hostname'] = '13.56.11.140';
 $db['default']['username'] = 'Diatel';
-$db['default']['password'] = 'diatel';
+$db['default']['password'] = 'diatel2020*';
 $db['default']['database'] = 'superrecargafundacion';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
